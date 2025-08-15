8c018f3149baf3113624b83cdac452d78bf559e9
jesulini <jesulini14@gmail.com>
2025-08-15 14:48:12 -0500

    cambios en el pom.xml creacion de dos plugins para maven con Junit 5 y creacion del reporte del testeo a la clase Calculadora usando Junit 5

--- demo/src/test/java/CalculadoraTest.java
+++ demo/src/test/java/CalculadoraTest.java
@@ -1,28 +1,30 @@
+import com.example.Calculadora;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Calculadora_test {
-    calculadora calc = new calculadora();
+public class CalculadoraTest {
+
+    Calculadora calc = new Calculadora();
 
     @Test
     public void testSumar() {
         assertEquals(5, calc.sumar(2, 3));
-        }
+    }
 
     @Test
     public void testRestar() {
         assertEquals(1, calc.restar(3, 2));
-        }
+    }
 
     @Test
     public void testMultiplicar() {
         assertEquals(6, calc.multiplicar(2, 3));
-        }
+    }
 
     @Test
     public void testDividir() {
-        assertEquals(2.0, calc.dividir(6, 3));
-        }
+        assertEquals(2, calc.dividir(6, 3));
+    }
 
     @Test
     public void testDividirPorCero() {
